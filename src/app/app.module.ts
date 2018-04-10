0ff0f05b65af6250361e3855401d0398ccd3c244
Akhouri Vishal Sinha <vakhouri@gmail.com>
2018-04-11 00:18:35 +1000

    Add create expenses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CreateExpenseComponent } from './components/create-expense/create-expense.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateExpenseComponent
   ],
   imports: [
     BrowserModule
